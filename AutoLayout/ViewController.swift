e12c75d013a8d66865adc88f0a9aaee9aa2ca382
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-03-05 15:32:27 +0600

    set constraint using programatically.

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var swooshLogo: UIImageView!
+    
+    @IBOutlet weak var BGImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello")
+        
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.width / 2, y: 50, width: swooshLogo.frame.width, height: swooshLogo.frame.height)
+        BGImg.frame = view.frame
     }
 
-
+    
+    
+    
 }
 
